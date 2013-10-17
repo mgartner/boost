d4acff0e3f00c7a3f2309728010e7a2ae35c5a95
Marcus Gartner <marcus@fanium.com>
2013-10-16 20:59:04 -0700

    afmotion and stuff

--- app/views/dyno_table_view/dyno_table_view_cell.rb
+++ app/views/dyno_table_view/dyno_table_view_cell.rb
@@ -3,25 +3,23 @@ class DynoTableViewCell < UITableViewCell
 
   def initWithStyle(style, reuseIdentifier: identifier)
     super.tap do
-
       self.accessoryType = UITableViewCellAccessoryDisclosureIndicator
 
-      @title = UILabel.alloc.initWithFrame(CGRectMake(15, 8, 215, 22))
-      @title.font = UIFont.fontWithName("HelveticaNeue-Light", size: 18)
+      @title = UILabel.alloc.initWithFrame(CGRectMake(15, 6, 215, 24))
+      @title.font = UIFont.fontWithName("HelveticaNeue-Light", size: 20)
       self.contentView.addSubview @title
 
-      @command = UILabel.alloc.initWithFrame(CGRectMake(15, 33, 215, 20))
-      @command.font = UIFont.fontWithName("CourierNewPSMT", size: 14)
-      @command.textColor = "#555".to_color
+      @command = UILabel.alloc.initWithFrame(CGRectMake(15, 35, 215, 20))
+      @command.font = UIFont.fontWithName("CourierNewPSMT", size: 16)
       self.contentView.addSubview @command
       
-      @dyno_number = UILabel.alloc.initWithFrame(CGRectMake(240, 8, 40, 22))
-      @dyno_number.font = UIFont.fontWithName("HelveticaNeue-Light", size: 18)
+      @dyno_number = UILabel.alloc.initWithFrame(CGRectMake(240, 6, 40, 24))
+      @dyno_number.font = UIFont.fontWithName("HelveticaNeue-Light", size: 20)
       @dyno_number.textAlignment = UITextAlignmentRight
       self.contentView.addSubview @dyno_number
 
-      @dyno_size = UILabel.alloc.initWithFrame(CGRectMake(240, 32, 40, 20))
-      @dyno_size.font = UIFont.fontWithName("HelveticaNeue-Medium", size: 14)
+      @dyno_size = UILabel.alloc.initWithFrame(CGRectMake(240, 35, 40, 20))
+      @dyno_size.font = UIFont.fontWithName("HelveticaNeue-Medium", size: 16)
       @dyno_size.textAlignment = UITextAlignmentRight
       self.contentView.addSubview @dyno_size
 
@@ -33,7 +31,6 @@ class DynoTableViewCell < UITableViewCell
     @command.text = dyno.command
     @dyno_size.text = "#{dyno.size.to_s}x"
     @dyno_number.text = dyno.quantity.to_s
-
     if dyno.size == 1
       @dyno_size.textColor = "#8e44ad".to_color
     elsif dyno.size == 2
